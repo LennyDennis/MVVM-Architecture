8a0a586de11e615e8144f4ce7a57a4da34af6d02
LennyDennis <lennydennis@gmail.com>
2020-07-20 20:33:15 +0300

    Add a add note activity to add notes

---
+++ app/src/main/java/com/lennydennis/mvvmarchitecture/AddNoteActivity.java
@@ -0,0 +1,82 @@
+package com.lennydennis.mvvmarchitecture;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    public static final String EXTRA_TITLE =
+            "com.lennydennis.mvvmarchitecture.EXTRA_TITLE";
+    public static final String EXTRA_DESCRIPTION =
+            "com.lennydennis.mvvmarchitecture.EXTRA_DESCRIPTION";
+    public static final String EXTRA_PRIORITY=
+            "com.lennydennis.mvvmarchitecture.EXTRA_PRIORITY";
+    private EditText editNoteTitle;
+    private EditText editNotedDescription;
+    private NumberPicker noteNumberPicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        editNoteTitle = findViewById(R.id.et_note_title);
+        editNotedDescription = findViewById(R.id.et_note_description);
+        noteNumberPicker = findViewById(R.id.note_priority);
+
+        noteNumberPicker.setMinValue(0);
+        noteNumberPicker.setMaxValue(10);
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_close_24);
+        setTitle("Add Note");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.save_note:
+                saveNote();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        String title = editNoteTitle.getText().toString();
+        String description = editNotedDescription.getText().toString();
+        int priority = noteNumberPicker.getValue();
+
+        if(title.trim().isEmpty() || description.trim().isEmpty()){
+            Toast.makeText(this, "Insert all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_TITLE,title);
+        intent.putExtra(EXTRA_DESCRIPTION,description);
+        intent.putExtra(EXTRA_PRIORITY,priority);
+
+        setResult(RESULT_OK, intent);
+        finish();
+
+    }
+}
--- app/src/main/java/com/lennydennis/mvvmarchitecture/MainActivity.java
+++ app/src/main/java/com/lennydennis/mvvmarchitecture/MainActivity.java
@@ -1,18 +1,24 @@
 package com.lennydennis.mvvmarchitecture;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.lennydennis.mvvmarchitecture.Model.Note;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
     private NoteViewModel mNoteViewModel;
 
     @Override
@@ -24,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_note_btn);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
         final NoteAdapter noteAdapter = new NoteAdapter();
         recyclerView.setAdapter(noteAdapter);
 
@@ -34,5 +49,23 @@ public class MainActivity extends AppCompatActivity {
                 noteAdapter.setNotes(notes);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            String title = data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description = data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority = data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note = new Note(title,description,priority);
+            mNoteViewModel.insert(note);
+
+            Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Note Not Saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
